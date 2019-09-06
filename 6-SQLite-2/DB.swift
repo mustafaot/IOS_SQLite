514002941bb28675f2387d6ecd513d5031f7cd13
Mustafa OT <mustafa.ot@GLTMAC105.local>
2019-09-06 02:02:30 -0700

    mot - sql lite first commit

---
+++ 6-SQLite-2/DB.swift
@@ -0,0 +1,9 @@
+//
+//  DB.swift
+//  6-SQLite-2
+//
+//  Created by Mustafa OT on 4.09.2019.
+//  Copyright © 2019 HAKAN ÖZER. All rights reserved.
+//
+
+import Foundation
---
+++ 6-SQLite-2/Splash.swift
@@ -0,0 +1,30 @@
+//
+//  Splash.swift
+//  6-SQLite-2
+//
+//  Created by Mustafa OT on 5.09.2019.
+//  Copyright © 2019 HAKAN ÖZER. All rights reserved.
+//
+
+import UIKit
+
+class Splash: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
